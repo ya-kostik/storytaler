daf64cafe2d81144c67c206f099721b2e6d18b7f
Constantin Tsukanov <c@indotech.ru>
2024-03-21 12:47:23 +0300

    Typo is fixed

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/storyteller/',
 	css: {
 		postcss: {
 			plugins: [tailwindcss(), autoprefixer()],
